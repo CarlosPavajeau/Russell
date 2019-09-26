96dd99b4565611f00640637792dd0fd118cbfac3
Carlos Pavajeau <cantte098@gmail.com>
2019-09-26 11:41:47 -0500

    Add new class 'AccessData'

---
+++ src/Entity/AccessData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Entity
+{
+    public class AccessData
+    {
+        private string user;
+        private string password;
+
+        public AccessData(string user, string password)
+        {
+
+        }
+
+        public string User
+        {
+            get
+            {
+                return user;
+            }
+            set
+            {
+                user = (value != string.Empty) ? value : throw new ArgumentException("The user is invalid");
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = (value != string.Empty) ? value : throw new ArgumentException("The password is invalid");
+            }
+        }
+    }
+}
