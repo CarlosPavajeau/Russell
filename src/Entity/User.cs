1c235d30ce81e48ecc32be00dc67b1999e95e7de
Carlos Pavajeau <cantte098@gmail.com>
2019-09-26 11:42:13 -0500

    Add new class 'User'

---
+++ src/Entity/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public enum AccessLevel
+    {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public class User
+    {
+        public User(AccessData accessData, AccessLevel accessLevel)
+        {
+            AccessData = accessData;
+            AccessLevel = accessLevel;
+        }
+
+        public AccessData AccessData
+        {
+            get;
+            set;
+        }
+
+        public AccessLevel AccessLevel
+        {
+            get;
+            set;
+        }
+
+        public bool IsLowUser()
+        {
+            return AccessLevel == AccessLevel.LOW;
+        }
+
+        public bool IsMediumUser()
+        {
+            return AccessLevel == AccessLevel.MEDIUM;
+        }
+
+        public bool IsHighUser()
+        {
+            return AccessLevel == AccessLevel.HIGH;
+        }
+    }
+}
