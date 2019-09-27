87d4c1e80d7084939a3faa29e29b407a4c965b58
Carlos Pavajeau <cantte098@gmail.com>
2019-09-27 17:01:10 -0500

    Create new classes 'Imprint' and 'LegalInformation' #1

---
+++ src/Entity/Imprint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Entity
+{
+    public class Imprint
+    {
+        private string engineNumber;
+        private string chassisNumber;
+
+        public Imprint(string engineNumber, string chassisNumber)
+        {
+            EngineNumber = engineNumber;
+            ChassisNumber = chassisNumber;
+        }
+
+        public string EngineNumber
+        {
+            get
+            {
+                return engineNumber;
+            }
+            set
+            {
+                engineNumber = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The engine number is invalid");
+            }
+        }
+
+        public string ChassisNumber
+        {
+            get
+            {
+                return chassisNumber;
+            }
+            set
+            {
+                chassisNumber = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The chassis number is invalid");
+            }
+        }
+
+    }
+}
---
+++ src/Entity/LegalInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Entity
+{
+    public class LegalInformation
+    {
+        public LegalInformation(DateTime dueDate, DateTime dateOfRenovation)
+        {
+            DueDate = dueDate;
+            DateOfRenovation = dateOfRenovation;
+        }
+
+        public DateTime DueDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateOfRenovation
+        {
+            get;
+            set;
+        }
+    }
+}
