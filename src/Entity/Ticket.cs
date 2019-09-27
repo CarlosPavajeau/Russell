d916ae5c847818722f22fcf60a08312666909676
Carlos Pavajeau <cantte098@gmail.com>
2019-09-27 17:03:40 -0500

    Create new class 'Ticket'

---
+++ src/Entity/Ticket.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class Ticket
+    {
+        private string number;
+        private string destination;
+        private decimal price;
+
+        public Ticket(string number, string destination, decimal price, Person client, Vehicle vehicle)
+        {
+            Number = number;
+            Destination = destination;
+            Price = price;
+            Client = client;
+            Vehicle = vehicle;
+
+        }
+
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The number is invalid");
+            }
+        }
+
+        public string Destination
+        {
+            get
+            {
+                return destination;
+            }
+            set
+            {
+                destination = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The destination is invalid");
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = (value > 0) ? value : throw new ArgumentException("The price is invalid");
+            }
+        }
+
+        public Person Client
+        {
+            get;
+            set;
+        }
+
+        public Vehicle Vehicle
+        {
+            get;
+            set;
+        }
+    }
+}
