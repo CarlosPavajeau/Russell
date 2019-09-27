903d81f3b678b554bba037e7e45b3719abbf44b0
Carlos Pavajeau <cantte098@gmail.com>
2019-09-27 17:05:36 -0500

    Add new classes: 'FinancialInformation' and 'TransportForm'

---
+++ src/Entity/TransportForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class TransportForm
+    {
+        public TransportForm()
+        {
+            Tickets = new List<Ticket>();
+        }
+        public List<Ticket> Tickets
+        {
+            get;
+            set;
+        }
+
+        public FinalcialInformation FinalcialInformation
+        {
+            get;
+            set;
+        }
+
+        public Person Dispatcher
+        {
+            get;
+            set;
+        }
+
+        public void AddTicket(Ticket ticket)
+        {
+            Tickets.Add(ticket);
+        }
+    }
+}
