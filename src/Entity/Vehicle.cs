7aa63203b6305096fc0ee30ae75e7e002bd007f0
Carlos Pavajeau <cantte098@gmail.com>
2019-09-27 17:02:52 -0500

    Add new classes: 'VehicleFeatures' and 'Vehicle' #1

---
+++ src/Entity/Vehicle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Entity
+{
+    public class Vehicle
+    {
+        private string licensePlate;
+        private string internalNumber;
+        private string propertyCardNumber;
+
+        LegalInformation license;
+        LegalInformation soat;
+        LegalInformation operationCard;
+        LegalInformation technoMechanicalReview;
+        LegalInformation biMonthlyReview;
+
+        int chairs;
+
+        public string LicensePlate
+        {
+            get
+            {
+                return licensePlate;
+            }
+            set
+            {
+                licensePlate = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The license palte is invalid");
+            }
+        }
+
+        public string InternalNumber
+        {
+            get
+            {
+                return internalNumber;
+            }
+            set
+            {
+                internalNumber = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The internal number is invalid");
+            }
+        }
+
+        public string PropertyCardNumber
+        {
+            get
+            {
+                return propertyCardNumber;
+            }
+            set
+            {
+                propertyCardNumber = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The property card number is invalid");
+            }
+        }
+
+        public int Chairs
+        {
+            get
+            {
+                return chairs;
+            }
+            set
+            {
+                chairs = (value > 0) ? value : throw new ArgumentException("The chairs value is invalid"); 
+            }
+        }
+
+        public bool Stade
+        {
+            get;
+            set;
+        }
+
+        public Person Owner
+        {
+            get;
+            set;
+        }
+
+        public Person Driver
+        {
+            get;
+            set;
+        }
+
+        public Imprint Imprint
+        {
+            get;
+            set;
+        }
+
+        public VehicleFeatures Features
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ src/Entity/VehicleFeatures.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Entity
+{
+    public class VehicleFeatures
+    {
+        private string type;
+        private string mark;
+        private string model;
+        private string modelNumber;
+        private string color;
+        
+        public VehicleFeatures(string type, string mark, string model, string modelNumber, string color)
+        {
+            Type = type;
+            Mark = mark;
+            Model = model;
+            ModelNumber = modelNumber;
+            Color = color;
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The type is invalid");
+            }
+        }
+
+        public string Mark
+        {
+            get
+            {
+                return mark;
+            }
+            set
+            {
+                mark = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The mark is invalid");
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The model is invalid");
+            }
+        }
+
+        public string ModelNumber
+        {
+            get
+            {
+                return modelNumber;
+            }
+            set
+            {
+                modelNumber = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The model number is invalid");
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The color is invalid");
+            }
+        }
+    }
+}
