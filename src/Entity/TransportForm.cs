40ad7b1d9a9fc8f68756cff261408e61396c2bf0
Carlos Pavajeau <cantte098@gmail.com>
2019-09-28 18:44:35 -0500

    Finish the class 'TransportForm' #1 Closes #1

--- src/Entity/TransportForm.cs
+++ src/Entity/TransportForm.cs
@@ -6,31 +6,73 @@ namespace Entity
 {
     public class TransportForm
     {
+        private string number;
+        private string originCity;
+        private string destinationCity;
+
         public TransportForm()
         {
             Tickets = new List<Ticket>();
         }
-        public List<Ticket> Tickets
+
+        public string Number
         {
-            get;
-            set;
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The number is invalid");
+            }
         }
 
-        public FinalcialInformation FinalcialInformation
+        public string OriginCity
         {
-            get;
-            set;
+            get
+            {
+                return originCity;
+            }
+            set
+            {
+                originCity = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The origin city is invalid");
+            }
         }
 
-        public Person Dispatcher
+        public string DestinationCity
         {
-            get;
-            set;
+            get
+            {
+                return destinationCity;
+            }
+            set
+            {
+                destinationCity = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The number is invalid");
+            }
         }
+
+        public decimal ValueOfTickets { get; private set; }
+
+        public decimal TotalValue { get; private set; }
+        public DateTime Date { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+        public List<Ticket> Tickets { get; set; }
+
+        public FinalcialInformation FinalcialInformation { get; set; }
+
+        public Person Dispatcher { get; set; }
 
         public void AddTicket(Ticket ticket)
         {
             Tickets.Add(ticket);
+            ValueOfTickets += ticket.Price;
+            UpdateTotalValue(ticket.Price);
+        }
+
+        public void UpdateTotalValue(decimal amount)
+        {
+            TotalValue += amount;
         }
     }
 }
