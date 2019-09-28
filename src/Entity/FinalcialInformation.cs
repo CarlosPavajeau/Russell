a29a35316a44a0c83a440774946f1adda0c0f096
Carlos Pavajeau <cantte098@gmail.com>
2019-09-28 18:42:36 -0500

    Add properties for class 'FinalcialInfomation' #1

--- src/Entity/FinalcialInformation.cs
+++ src/Entity/FinalcialInformation.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Entity
 {
     public class FinalcialInformation
     {
+        public FinalcialInformation()
+        {
+
+        }
+
+        public decimal ReplacementFund { get; set; }
+
+        public decimal SocialContribution { get; set; }
+
+        public decimal TireService { get; set; }
+
+        public decimal VehicleFixService { get; set; }
+
+        public decimal NonContractualSecureService { get; set; }
+
+        public decimal ConstactInsuranceService { get; set; }
+
+        public decimal SocialProtection { get; set; }
+
+        public decimal ExtraordinaryProtection { get; set; }
+
+        public decimal Administration { get; set; }
+
+        public decimal Others { get; set; }
     }
 }
