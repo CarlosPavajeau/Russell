bab6388779f469ddb95b43081b272df90babc10f
Carlos Pavajeau <cantte098@gmail.com>
2019-10-07 16:02:50 -0500

    Changes in class 'TransportForm' Add constructors Add new methods 'AddTikect', 'UpdateTotalValue' and 'CalculateFinalcialInformation' Clean class

--- src/Entity/TransportForm.cs
+++ src/Entity/TransportForm.cs
@@ -5,62 +5,89 @@ namespace Entity
 {
     public class TransportForm
     {
-        private string originCity;
-        private string destinationCity;
+        private static int transportFormsCount = 0;
 
+        public TransportForm(string number, string originCity, string destinationCity, Vehicle vehicle, Person dispatcher)
+        {
+            Number = number;
+            OriginCity = originCity;
+            DestinationCity = destinationCity;
+            Vehicle = vehicle;
+            Dispatcher = dispatcher;
+            Tickets = new List<Ticket>();
+            FinalcialInformation = new FinalcialInformation();
+        }
+
+        public TransportForm(string originCity, string destinationCity, Vehicle vehicle, Person dispatcher)
+        {
+            Number = (++transportFormsCount).ToString("00000");
+            OriginCity = originCity;
+            DestinationCity = destinationCity;
+            Vehicle = vehicle;
+            Dispatcher = dispatcher;
+            Date = DateTime.Now;
+            Tickets = new List<Ticket>();
+            FinalcialInformation = new FinalcialInformation();
+        }
         public TransportForm()
         {
+            Number = (++transportFormsCount).ToString("00000");
+            Date = DateTime.Now;
             Tickets = new List<Ticket>();
+            FinalcialInformation = new FinalcialInformation();
         }
 
         public string Number { get; }
 
-        public string OriginCity
-        {
-            get
-            {
-                return originCity;
-            }
-            set
-            {
-                originCity = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The origin city is invalid");
-            }
-        }
-
-        public string DestinationCity
-        {
-            get
-            {
-                return destinationCity;
-            }
-            set
-            {
-                destinationCity = (!string.IsNullOrEmpty(value)) ? value : throw new ArgumentException("The number is invalid");
-            }
-        }
-
+        public string OriginCity { get; set; }
+        public string DestinationCity { get; set; }
         public decimal ValueOfTickets { get; private set; }
 
         public decimal TotalValue { get; private set; }
-        public DateTime Date { get; set; }
 
+        public DateTime Date { get; set; }
         public DateTime DepartureTime { get; set; }
         public List<Ticket> Tickets { get; }
 
         public FinalcialInformation FinalcialInformation { get; set; }
 
         public Person Dispatcher { get; set; }
+        public Vehicle Vehicle { get; set; }
 
-        public void AddTicket(Ticket ticket)
+        private void AddTicket(Ticket ticket)
         {
             Tickets.Add(ticket);
             ValueOfTickets += ticket.Price;
-            UpdateTotalValue(ticket.Price);
+            UpdateTotalValue();
         }
 
-        public void UpdateTotalValue(decimal amount)
+        public void AddTicket(Person client, decimal price)
         {
-            TotalValue += amount;
+            Ticket ticket = new Ticket($"{Number}-{(Tickets.Count + 1).ToString("00000")}", client, Vehicle, DestinationCity, price);
+            AddTicket(ticket);
+        }
+
+        public void UpdateTotalValue()
+        {
+            TotalValue = CalculateFinalcialInformation() + ValueOfTickets;
+        }
+
+        private decimal CalculateFinalcialInformation()
+        {
+            decimal accumulatedValue = 0;
+
+            accumulatedValue += FinalcialInformation.Administration;
+            accumulatedValue += FinalcialInformation.ConstactInsuranceService;
+            accumulatedValue += FinalcialInformation.ExtraordinaryProtection;
+            accumulatedValue += FinalcialInformation.NonContractualSecureService;
+            accumulatedValue += FinalcialInformation.Others;
+            accumulatedValue += FinalcialInformation.ReplacementFund;
+            accumulatedValue += FinalcialInformation.SocialContribution;
+            accumulatedValue += FinalcialInformation.SocialProtection;
+            accumulatedValue += FinalcialInformation.TireService;
+            accumulatedValue += FinalcialInformation.VehicleFixService;
+
+            return accumulatedValue;
         }
     }
 }
