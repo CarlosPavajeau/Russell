efdc079e40651d0a56131fb00269b1aeb548da96
Carlos Pavajeau <cantte098@gmail.com>
2019-10-10 09:23:51 -0500

    Add 'get' and 'set' in the properties

--- src/Entity/FinalcialInformation.cs
+++ src/Entity/FinalcialInformation.cs
@@ -1,31 +1,75 @@
-﻿
+﻿using System;
+
 namespace Entity
 {
     public class FinalcialInformation
     {
+        private decimal _replacementFund, _socialContribution, _tireService,
+                        _vehicleFixService, _nonContractualSecureService, _constactInsuranceService,
+                        _socialProtection, _extraordinaryProtection, _administration, _others;
         public FinalcialInformation()
         {
         }
 
-        public decimal ReplacementFund { get; set; }
+        public decimal ReplacementFund
+        {
+            get => _replacementFund;
+            set => _replacementFund = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal SocialContribution { get; set; }
+        public decimal SocialContribution
+        {
+            get => _socialContribution;
+            set => _socialContribution = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal TireService { get; set; }
+        public decimal TireService
+        {
+            get => _tireService;
+            set => _tireService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal VehicleFixService { get; set; }
+        public decimal VehicleFixService
+        {
+            get => _vehicleFixService;
+            set => _vehicleFixService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal NonContractualSecureService { get; set; }
+        public decimal NonContractualSecureService
+        {
+            get => _nonContractualSecureService;
+            set => _nonContractualSecureService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal ConstactInsuranceService { get; set; }
+        public decimal ConstactInsuranceService
+        {
+            get => _constactInsuranceService;
+            set => _constactInsuranceService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal SocialProtection { get; set; }
+        public decimal SocialProtection
+        {
+            get => _socialProtection;
+            set => _socialProtection = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal ExtraordinaryProtection { get; set; }
+        public decimal ExtraordinaryProtection
+        {
+            get => _extraordinaryProtection;
+            set => _extraordinaryProtection = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal Administration { get; set; }
+        public decimal Administration
+        {
+            get => _administration;
+            set => _administration = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
-        public decimal Others { get; set; }
+        public decimal Others
+        {
+            get => _others;
+            set => _others = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+        }
 
         public decimal CalculateTotal()
         {
