d6fcb952490d6030254f698f92cbae2ac496c253
Carlos Pavajeau <cantte098@gmail.com>
2019-11-02 20:30:42 -0500

    Entity: Add new class 'Route'

---
+++ src/Entity/Route.cs
@@ -0,0 +1,23 @@
+﻿namespace Entity
+{
+    public class Route
+    {
+        public Route()
+        {
+
+        }
+
+        public Route(string originCity, string destinationCity, decimal cost)
+        {
+            OriginCity = originCity;
+            DestinationCity = destinationCity;
+            Cost = cost;
+        }
+
+        public string OriginCity { get; set; }
+
+        public string DestinationCity { get; set; }
+
+        public decimal Cost { get; set; }
+    }
+}
