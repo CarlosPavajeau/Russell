73a3f043bc6324ad96e3ac7b95389afa61d576df
Carlos Pavajeau <cantte098@gmail.com>
2019-11-03 11:37:30 -0500

    Add new class 'ConnectedObject'

---
+++ src/Entity/Common/ConnectedObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Entity.Common
+{
+    public sealed class ConnectedObject
+    {
+        public ConnectedObject(Socket socket, Message message)
+        {
+            Socket = socket;
+            Message = message;
+        }
+
+        public ConnectedObject(Socket socket)
+        {
+            Socket = socket;
+            Message = new Message();
+        }
+
+        public Socket Socket { get; set; }
+
+        public Message Message { get; set; }
+
+        public void Close()
+        {
+            try
+            {
+                Socket.Shutdown(SocketShutdown.Both);
+                Socket.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
