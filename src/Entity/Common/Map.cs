89e57e037955a1bfb3bdd9e0a29213083a18d113
Carlos Pavajeau <cantte098@gmail.com>
2019-11-03 11:37:11 -0500

    Add new class 'Map'

---
+++ src/Entity/Common/Map.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Entity.Common
+{
+    public static class Map
+    {
+        public static Message Serialize(object anObject)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(memoryStream, anObject);
+                return new Message(memoryStream.ToArray());
+            }
+        }
+
+        public static object Deserialize(Message message)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(message.Data))
+                return new BinaryFormatter().Deserialize(memoryStream);
+        }
+    }
+}
