dd5c619fe3ef866de34d2eca586c44bafdf8accd
Carlos Pavajeau <cantte098@gmail.com>
2019-11-03 11:36:37 -0500

    Add new class 'Message'

---
+++ src/Entity/Common/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace Entity.Common
+{
+    public sealed class Message
+    {
+        public Message(byte[] data)
+        {
+            Data = data;
+        }
+
+        public Message()
+        {
+            Data = new byte[ConnectionSettings.ByteBufferSize];
+        }
+        public byte[] Data { get; set; }
+    }
+}
