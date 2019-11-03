5a5a8790695de0671bd0812e2d6f8dfbc255019e
Carlos Pavajeau <cantte098@gmail.com>
2019-11-02 19:12:20 -0500

    View: Some improvements

--- src/View/MainWindow.xaml.cs
+++ src/View/MainWindow.xaml.cs
@@ -7,9 +7,6 @@ namespace View
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
     }
 }
