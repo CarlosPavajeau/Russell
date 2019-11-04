e21363c463b3061de0b84f2ae72d8b7f93035c1d
Carlos Pavajeau <cantte098@gmail.com>
2019-11-03 19:25:33 -0500

    Add new class 'Employee'

---
+++ src/Entity/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Entity
+{
+    public class Employee : Person
+    {
+        public Employee(string id, string firstName, string secondName, string lastName, string secondLastName,
+                        string cellphone, string email, string address) : base(id, firstName, secondName, lastName, secondLastName)
+        {
+            Cellphone = cellphone;
+            Email = email;
+            Address = address;
+        }
+
+        public string Cellphone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
