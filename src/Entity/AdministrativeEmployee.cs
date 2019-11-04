a51f300d05bf3cdf24b14d910e2672633943ea84
Carlos Pavajeau <cantte098@gmail.com>
2019-11-03 19:26:29 -0500

    Add new class 'AdministrativeEmployee'

---
+++ src/Entity/AdministrativeEmployee.cs
@@ -0,0 +1,14 @@
+﻿namespace Entity
+{
+    public class AdministrativeEmployee : Employee
+    {
+        public AdministrativeEmployee(string id, string firstName, string secondName, string lastName, string secondLastName,
+                                      string cellphone, string email, string address, User user) : base(id, firstName, secondName, lastName, secondLastName,
+                                                                                                        cellphone, email, address)
+        {
+            User = user;
+        }
+
+        public User User { get; set; }
+    }
+}
