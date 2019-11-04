b85e24e8d890678b64d96f2953dbc817c0522bf2
Carlos Pavajeau <cantte098@gmail.com>
2019-11-03 19:26:04 -0500

    Add new class 'Passenger'

---
+++ src/Entity/Passenger.cs
@@ -0,0 +1,11 @@
+﻿namespace Entity
+{
+    public class Passenger : Person
+    {
+        public Passenger(string id, string firstName, string secondName, string lastName, string secondLastName) :
+                         base(id, firstName, secondName, lastName, secondLastName)
+        {
+
+        }
+    }
+}
