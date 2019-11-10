0172569890c433532cd7e5dc850a9f81bd3c8bb2
Carlos Pavajeau <cantte098@gmail.com>
2019-11-09 21:43:55 -0500

    View: Build MainWindow app

--- src/View/MainWindow.xaml.cs
+++ src/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace View
 {
@@ -8,5 +10,21 @@ namespace View
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ShowVehiclesOptions(object sender, RoutedEventArgs e)
+        {
+            SetMainPanel(new VehiclePanel());
+        }
+
+        public void SetMainPanel(UserControl userControl)
+        {
+            MainGrid.Children.Clear();
+            MainGrid.Children.Add(userControl);
+        }
     }
 }
