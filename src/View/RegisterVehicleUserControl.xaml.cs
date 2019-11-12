c825c30440bb1eaf2c54c2d11a0e55ee41b33947
Carlos Pavajeau <cantte098@gmail.com>
2019-11-12 07:43:44 -0500

    View/Vehicle: Rename and delete some files

---
+++ src/View/RegisterVehicleUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for RegisterVehicleUserControl.xaml
+    /// </summary>
+    public partial class RegisterVehicleUserControl : UserControl
+    {
+        public RegisterVehicleUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/View/VehicleUserControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for VehicleUserControl.xaml
+    /// </summary>
+    public partial class VehicleUserControl : UserControl
+    {
+        public VehicleUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VehicleMainGrid.Children.Clear();
+            VehicleMainGrid.Children.Add(new RegisterVehicleUserControl());
+        }
+
+        private void RegisterVehicleButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPanel.Children.Clear();
+            MainPanel.Children.Add(new RegisterVehicleUserControl());
+        }
+
+        private void SearchVehicleButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPanel.Children.Clear();
+            MainPanel.Children.Add(new SearchVehicleUserControl());
+        }
+    }
+}
