aff3afa8785cc8fac60c8d7d3b3d825e3141c593
Carlos Pavajeau <cantte098@gmail.com>
2019-11-12 16:19:45 -0500

    View: Add BankDraftUserControl

---
+++ src/View/BankDraftUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for BankDraftUserControl.xaml
+    /// </summary>
+    public partial class BankDraftUserControl : UserControl
+    {
+        public BankDraftUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
