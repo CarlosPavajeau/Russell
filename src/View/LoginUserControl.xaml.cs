e8c9126c0192c3d707fd4ef147d0e1c3341de044
Carlos Pavajeau <cantte098@gmail.com>
2019-11-12 07:42:28 -0500

    View: Rename files

---
+++ src/View/LoginUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for LoginUserControl.xaml
+    /// </summary>
+    public partial class LoginUserControl : UserControl
+    {
+        public LoginUserControl(LoginAction action)
+        {
+            InitializeComponent();
+            Action = action;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Action?.Invoke();
+        }
+
+        public delegate void LoginAction();
+
+        private readonly LoginAction Action;
+    }
+}
