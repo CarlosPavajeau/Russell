0a7765747d7946c6cee3bf5bfca06309737ebb3b
Carlos Pavajeau <cantte098@gmail.com>
2019-11-12 16:19:16 -0500

    View: Add classes for magane register people

---
+++ src/View/RegisterPersonUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for RegisterPersonUserControl.xaml
+    /// </summary>
+    public partial class RegisterPersonUserControl : UserControl
+    {
+        public RegisterPersonUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
