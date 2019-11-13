9be7593aaf73cc56e35b2e0c5044f58eee10d39c
Carlos Pavajeau <cantte098@gmail.com>
2019-11-13 16:35:01 -0500

    Add new search forms #3

---
+++ src/View/SearchCommendUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for SearchCommendUserControl.xaml
+    /// </summary>
+    public partial class SearchCommendUserControl : UserControl
+    {
+        public SearchCommendUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchFields_Loaded(object sender, RoutedEventArgs e)
+        {
+            SearchFields.SearchTitleField.Text = "Codido: ";
+        }
+    }
+}
