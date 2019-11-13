a4b598cd21f15259dcf026b03a7877b42bc7a35e
Carlos Pavajeau <cantte098@gmail.com>
2019-11-13 16:23:10 -0500

    View: Create a generic search panel

---
+++ src/View/SearchFields.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for SearchFields.xaml
+    /// </summary>
+    public partial class SearchFields : UserControl
+    {
+        public SearchFields()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void SearchAction(object sender, RoutedEventArgs e);
+
+        private SearchAction Search;
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            Search?.Invoke(sender, e);
+        }
+    }
+}
