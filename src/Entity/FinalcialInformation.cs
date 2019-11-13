3ebba73de40fbaa118480291722a61b146ea2708
Carlos Pavajeau <cantte098@gmail.com>
2019-11-13 17:17:19 -0500

    Entity/FinalcialInformation: Use a dictionary and a enum

--- src/Entity/FinalcialInformation.cs
+++ src/Entity/FinalcialInformation.cs
@@ -1,92 +1,39 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Entity
 {
+    public enum FinalcialInformationType
+    {
+        REPLACEMENT_FUND,
+        SOCIAL_CONTRIBUTION,
+        TIRE_SERVICE,
+        VEHICLE_FIX_SERVICE,
+        NON_CONTRACTIAL_SERCURE_SERVICE,
+        CONSTACT_INSURANCE_SERVICE,
+        SOCIAL_PROTECTION,
+        EXTRAORDINARY_PROTECTION,
+        ADMINISTRATION,
+        OTHERS
+    }
     public class FinalcialInformation
     {
-        private decimal _replacementFund, _socialContribution, _tireService,
-                        _vehicleFixService, _nonContractualSecureService, _constactInsuranceService,
-                        _socialProtection, _extraordinaryProtection, _administration, _others;
+        private readonly Dictionary<FinalcialInformationType, decimal> _financialInformation;
+
         public FinalcialInformation()
         {
+            _financialInformation = new Dictionary<FinalcialInformationType, decimal>();
         }
 
-        public decimal ReplacementFund
+        public decimal this[FinalcialInformationType type]
         {
-            get => _replacementFund;
-            set => _replacementFund = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal SocialContribution
-        {
-            get => _socialContribution;
-            set => _socialContribution = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal TireService
-        {
-            get => _tireService;
-            set => _tireService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal VehicleFixService
-        {
-            get => _vehicleFixService;
-            set => _vehicleFixService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal NonContractualSecureService
-        {
-            get => _nonContractualSecureService;
-            set => _nonContractualSecureService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal ConstactInsuranceService
-        {
-            get => _constactInsuranceService;
-            set => _constactInsuranceService = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal SocialProtection
-        {
-            get => _socialProtection;
-            set => _socialProtection = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal ExtraordinaryProtection
-        {
-            get => _extraordinaryProtection;
-            set => _extraordinaryProtection = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal Administration
-        {
-            get => _administration;
-            set => _administration = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
-        }
-
-        public decimal Others
-        {
-            get => _others;
-            set => _others = (value >= 0) ? value : throw new ArgumentException("No se admiten valores negativos");
+            get => _financialInformation[type];
+            set => _financialInformation[type] = value;
         }
 
         public decimal CalculateTotal()
         {
-            decimal accumulatedValue = 0;
-
-            accumulatedValue += Administration;
-            accumulatedValue += ConstactInsuranceService;
-            accumulatedValue += ExtraordinaryProtection;
-            accumulatedValue += NonContractualSecureService;
-            accumulatedValue += Others;
-            accumulatedValue += ReplacementFund;
-            accumulatedValue += SocialContribution;
-            accumulatedValue += SocialProtection;
-            accumulatedValue += TireService;
-            accumulatedValue += VehicleFixService;
-
-            return accumulatedValue;
+            return _financialInformation.Sum(fi => fi.Value);
         }
     }
 }
