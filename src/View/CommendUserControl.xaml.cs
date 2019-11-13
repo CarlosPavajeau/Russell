4cff609177d43186fc962941d3b2fa888a58716b
Carlos Pavajeau <cantte098@gmail.com>
2019-11-13 16:21:56 -0500

    View/CommendUserControl: Add events

--- src/View/CommendUserControl.xaml.cs
+++ src/View/CommendUserControl.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace View
 {
@@ -21,6 +11,18 @@ namespace View
         public CommendUserControl()
         {
             InitializeComponent();
+        }
+
+        private void RegisterCommendButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPanel.Children.Clear();
+            MainPanel.Children.Add(new RegisterCommendUserControl());
+        }
+
+        private void SearchCommendButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPanel.Children.Clear();
+            MainPanel.Children.Add(new SearchCommendUserControl());
         }
     }
 }
