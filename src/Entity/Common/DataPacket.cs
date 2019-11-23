6472c8999ffae446730bd7c2edebef67f38ac473
Carlos Pavajeau <cantte098@gmail.com>
2019-11-22 22:11:34 -0500

    Entity.Common: Add new class 'DataPacket' #4

---
+++ src/Entity/Common/DataPacket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Entity.Common
+{
+    [Serializable]
+    public sealed class DataPacket
+    {
+        public DataPacket(Command command, object data)
+        {
+            Command = command;
+            Data = data;
+        }
+
+        public Command Command { get; set; }
+
+        public object Data { get; set; }
+    }
+}
