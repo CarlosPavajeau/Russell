231bb775874550c5d55ef886c6e7949edca25ff6
Carlos Pavajeau <cantte098@gmail.com>
2019-11-24 20:22:07 -0500

    BusinessLogicLayer: Add code in the 'Client' class

--- src/BusinessLogicLayer/Client/Client.cs
+++ src/BusinessLogicLayer/Client/Client.cs
@@ -1,6 +1,93 @@
-﻿namespace BusinessLogicLayer.Client
+﻿using Entity.Common;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace BusinessLogicLayer.Client
 {
     public class Client
     {
+        public delegate void ProcessReceiveData(object data);
+        public delegate void ProcessServerAnswer(ServerAnswer answer);
+
+        public ProcessReceiveData ReceiveData;
+        public ProcessServerAnswer ServerAnswer;
+
+        public Client()
+        {
+
+        }
+
+        public ConnectedObject client;
+
+        public void Connect()
+        {
+            client = new ConnectedObject(ConnectionHandler.CreateSocket());
+
+            while (!client.Socket.Connected)
+            {
+                try
+                {
+                    client.Socket.Connect(ConnectionSettings.IPEndPoint);
+                }
+                catch (SocketException)
+                {
+
+                }
+            }
+
+            Thread receiveThread = new Thread(() => Receive());
+        }
+
+        private void Receive()
+        {
+            while (true)
+            {
+                try
+                {
+                    Message message = new Message();
+                    int bytesRead = client.Socket.Receive(message.ByteBuffer, SocketFlags.None);
+                    if (bytesRead > 0)
+                    {
+                        object receiveData = Map.Deserialize(message);
+
+                        if (receiveData is ServerAnswer answer)
+                            ServerAnswer?.Invoke(answer);
+                        else
+                            ReceiveData?.Invoke(receiveData);
+                    }
+                }
+                catch (SocketException)
+                {
+
+                    client.Close();
+                }
+                catch (Exception)
+                {
+
+                }
+
+
+            }
+        }
+
+        public void Send(object objectToSend)
+        {
+            client.Message = Map.Serialize(objectToSend);
+
+            try
+            {
+                client.Socket.BeginSend(client.Message.ByteBuffer, 0, ConnectionSettings.ByteBufferSize, SocketFlags.None, new AsyncCallback(SendCallBack), client);
+            }
+            catch (SocketException)
+            {
+
+            }
+        }
+
+        private void SendCallBack(IAsyncResult result)
+        {
+
+        }
     }
 }
