95057b68bfed2c405210c1eadfb784705af05682
Carlos Pavajeau <cantte098@gmail.com>
2019-11-27 17:47:34 -0500

    DataAccessLayer: Add new class 'Repository' #5

---
+++ src/DataAccessLayer/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Common;
+
+namespace DataAccessLayer
+{
+    public abstract class Repository
+    {
+        protected DbConnection dbConnection;
+
+        protected Repository(DbConnection connection)
+        {
+            dbConnection = connection;
+        }
+    }
+}
