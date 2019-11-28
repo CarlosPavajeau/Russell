253e4fe9d4c3e2b6b7bdf9dfc01a4b3881e76e04
Carlos Pavajeau <cantte098@gmail.com>
2019-11-28 10:10:59 -0500

    DataAccessLayer: Add EmployeeRepository #5

---
+++ src/DataAccessLayer/EmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Common;
+using Entity;
+
+namespace DataAccessLayer
+{
+    public class EmployeeRepository : PersonRepository, ISave<Employee>, ISearch<Employee>
+    {
+        public EmployeeRepository(DbConnection connection) : base(connection)
+        {
+
+        }
+
+        public bool Save(Employee data)
+        {
+            if (!base.Save(data))
+                return false;
+
+            using (var command = dbConnection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO employees(person_id, cellphone, email, address) " +
+                                      "VALUES(@person_id, @cellphone, @email, @address)";
+
+                command.Parameters.Add(CreateDbParameter(command, "@person_id", data.ID));
+                command.Parameters.Add(CreateDbParameter(command, "@cellphone", data.Cellphone));
+                command.Parameters.Add(CreateDbParameter(command, "@email", data.Email));
+                command.Parameters.Add(CreateDbParameter(command, "@address", data.Address));
+
+                command.ExecuteNonQuery();
+                return true;
+            }
+        }
+
+        Employee ISearch<Employee>.Search(string primaryKey)
+        {
+            return null;
+        }
+    }
+}
