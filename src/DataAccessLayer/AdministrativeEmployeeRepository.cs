fc7912a753080afc10628a6d756f43ebbe17c5fa
Carlos Pavajeau <cantte098@gmail.com>
2019-11-28 10:11:14 -0500

    DataAccessLayer: Add AdministrativeEmployeeRepository #5

---
+++ src/DataAccessLayer/AdministrativeEmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Common;
+using Entity;
+
+namespace DataAccessLayer
+{
+    public class AdministrativeEmployeeRepository : EmployeeRepository, ISave<AdministrativeEmployee>, ISearch<AdministrativeEmployee>
+    {
+        public AdministrativeEmployeeRepository(DbConnection connection) : base(connection)
+        {
+
+        }
+
+        public bool Save(AdministrativeEmployee data)
+        {
+            if (!base.Save(data))
+                return false;
+
+            using (var command = dbConnection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO administrative_employees(person_id, username, passwordname, type_user) " +
+                                      "VALUES(@person_id, @username, @passwordname, @type_user)";
+
+                command.Parameters.Add(CreateDbParameter(command, "@person_id", data.ID));
+                command.Parameters.Add(CreateDbParameter(command, "@username", data.User.AccessData.User));
+                command.Parameters.Add(CreateDbParameter(command, "@passwordname", data.User.AccessData.Password));
+                command.Parameters.Add(CreateDbParameter(command, "@type_user", data.User.TypeUser.ToString()[0]));
+
+                command.ExecuteNonQuery();
+                return true;
+            }
+        }
+
+        AdministrativeEmployee ISearch<AdministrativeEmployee>.Search(string primaryKey)
+        {
+            return null;
+        }
+    }
+}
