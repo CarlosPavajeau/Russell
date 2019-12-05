15c9316de44740809ad4bbac56593aafab239e18
Carlos Pavajeau <cantte098@gmail.com>
2019-12-04 19:51:59 -0500

    Entity.Common: Add values on ServerAnswer, ClientRequest, Command #4

--- src/Entity/Common/ClientRequest.cs
+++ src/Entity/Common/ClientRequest.cs
@@ -2,5 +2,11 @@
 {
     public enum ClientRequest
     {
+        GET_ALL_BANKDRAFTS,
+        GET_ALL_COMMENDS,
+        GET_ALL_ROUTES,
+        GET_ALL_TRANSPORT_FORMS,
+        GET_ALL_VEHICLES,
+        IS_IT_THE_FIRST_APPLICATION_START
     }
 }
