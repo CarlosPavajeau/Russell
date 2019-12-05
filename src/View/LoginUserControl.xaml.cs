fb4e113e737dc1aae4bccbb5902033e91fcfd92d
Carlos Pavajeau <cantte098@gmail.com>
2019-12-04 20:01:02 -0500

    View: Add login

--- src/View/LoginUserControl.xaml.cs
+++ src/View/LoginUserControl.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
+using Entity;
+using Entity.Common;
 
 namespace View
 {
@@ -14,11 +18,31 @@ namespace View
             Action = action;
         }
 
-        private void ExitButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void ExitButton_Click(object sender, RoutedEventArgs e) => Environment.Exit(0);
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            Action?.Invoke();
+            MainWindow.Client.ReceiveData = SetAdministrativeEmployee;
+            MainWindow.Client.ServerAnswer = HandleServerAnswer;
+
+            DataPacket dataPacket = new DataPacket(Command.SEARCH_ADMINISTRATIVE_EMPLOYEE, UserField.Text);
+
+            MainWindow.Client.Send(dataPacket);
+        }
+
+        private void SetAdministrativeEmployee(object data)
+        {
+            if (data is AdministrativeEmployee administrativeEmployee)
+            {
+                MainWindow.AdministrativeEmployee = administrativeEmployee;
+                Dispatcher.Invoke(new ThreadStart(() => Action?.Invoke()));
+            }
+        }
+
+        private void HandleServerAnswer(ServerAnswer answer)
+        {
+            if (answer == ServerAnswer.NOT_FOUND_DATA)
+                MessageBox.Show("Usuario no registrado", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
 
         public delegate void LoginAction();
