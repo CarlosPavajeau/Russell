c8e3aa9e43324933610829c9788a90336ea3c353
Carlos Pavajeau <cantte098@gmail.com>
2019-12-04 19:44:49 -0500

    DAL/AdministrativeEmployeeRepository: Implement missing methods

--- src/DataAccessLayer/AdministrativeEmployeeRepository.cs
+++ src/DataAccessLayer/AdministrativeEmployeeRepository.cs
@@ -3,7 +3,7 @@ using Entity;
 
 namespace DataAccessLayer
 {
-    public class AdministrativeEmployeeRepository : EmployeeRepository, ISave<AdministrativeEmployee>, ISearch<AdministrativeEmployee>
+    public class AdministrativeEmployeeRepository : EmployeeRepository, ISave<AdministrativeEmployee>, ISearch<AdministrativeEmployee>, IMap<AdministrativeEmployee>
     {
         static readonly string[] ADMINISTRATIVE_EMPLOYEE_FIELDS = { "@person_id", "@username", "@passwordname", "@type_user" };
         public AdministrativeEmployeeRepository(DbConnection connection) : base(connection)
@@ -37,7 +37,54 @@ namespace DataAccessLayer
 
         public new AdministrativeEmployee Search(string primaryKey)
         {
-            return null;
+            using (var command = dbConnection.CreateCommand())
+            {
+                command.CommandText = "SELECT ad.person_id, pe.first_name, pe.second_name, pe.last_name, pe.second_last_name, " +
+                    "em.cellphone, em.email, em.address, ad.username, ad.passwordname, ad.type_user FROM administrative_employees ad " +
+                    "JOIN employees em ON em.person_id = ad.person_id JOIN people pe ON pe.person_id = ad.person_id " +
+                    "WHERE ad.username = @username";
+
+                command.Parameters.Add(CreateDbParameter(command, "@username", primaryKey));
+
+                DbDataReader dbDataReader = command.ExecuteReader();
+                return Map(dbDataReader);
+            }
+        }
+
+        public new AdministrativeEmployee Map(DbDataReader dbDataReader)
+        {
+            if (!dbDataReader.Read())
+                return null;
+
+            string id, firstName, secondName, lastName, secondLastName,
+                   cellphone, email, address, username, passwordname;
+
+            id = dbDataReader.GetString(0);
+            firstName = dbDataReader.GetString(1);
+            secondName = dbDataReader.GetString(2);
+            lastName = dbDataReader.GetString(3);
+            secondLastName = dbDataReader.GetString(4);
+            cellphone = dbDataReader.GetString(5);
+            email = dbDataReader.GetString(6);
+            address = dbDataReader.GetString(7);
+            username = dbDataReader.GetString(8);
+            passwordname = dbDataReader.GetString(9);
+
+            string typeUserStr = dbDataReader.GetString(10);
+            TypeUser typeUser = (typeUserStr == "S") ? TypeUser.SUPERUSER : ((typeUserStr == "D") ? TypeUser.DISPATCHER : TypeUser.ADMIN);
+
+            return new AdministrativeEmployee(id, firstName, secondName, lastName, secondLastName, cellphone,
+                                              email, address, new User(username, passwordname, typeUser));
+        }
+
+        public bool IsEmpty()
+        {
+            using (var command = dbConnection.CreateCommand())
+            {
+                command.CommandText = "SELECT COUNT(*) FROM administrative_employees";
+
+                return ((int)command.ExecuteScalar()) > 0;
+            }
         }
     }
 }
