2808f9161077738a36d2685271474d0873db3f73
Carlos Pavajeau <cantte098@gmail.com>
2019-12-07 11:46:12 -0500

    BLL/Service: Modify connection sql string

--- src/BusinessLogicLayer/Service.cs
+++ src/BusinessLogicLayer/Service.cs
@@ -5,7 +5,7 @@ namespace BusinessLogicLayer
 {
     public abstract class Service
     {
-        const string CONNECTION_SQL_STR = @"Data Source=DESKTOP-ND2GLJC\SQLEXPRESS;Initial Catalog=db_cootransnevada;Integrated Security=True";
+        const string CONNECTION_SQL_STR = @"Data Source=DESKTOP-ND2GLJC\SQLEXPRESS;Initial Catalog=db_cootransnevada;Integrated Security=True; MultipleActiveResultSets=true;";
         const string CONNECTION_ORACLE_STR = @"User Id=cootransnevada; password=cootransnevada; Data Source=localhost:1521/xepdb1; Pooling=false;";
 
         protected DbConnection dbConnection;
