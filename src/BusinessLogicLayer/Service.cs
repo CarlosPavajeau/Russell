5b6323ce75cf16e42d8360ab7d554a5462ba9875
Carlos Pavajeau <cantte098@gmail.com>
2019-12-07 08:53:20 -0500

    BLL: Add new connection string

--- src/BusinessLogicLayer/Service.cs
+++ src/BusinessLogicLayer/Service.cs
@@ -5,13 +5,14 @@ namespace BusinessLogicLayer
 {
     public abstract class Service
     {
-        const string CONNECTION_STR = @"Data Source=DESKTOP-ND2GLJC\SQLEXPRESS;Initial Catalog=db_cootransnevada;Integrated Security=True";
+        const string CONNECTION_SQL_STR = @"Data Source=DESKTOP-ND2GLJC\SQLEXPRESS;Initial Catalog=db_cootransnevada;Integrated Security=True";
+        const string CONNECTION_ORACLE_STR = @"User Id=cootransnevada; password=cootransnevada; Data Source=localhost:1521/xepdb1; Pooling=false;";
 
         protected DbConnection dbConnection;
 
         public Service()
         {
-            dbConnection = new SqlConnection(CONNECTION_STR);
+            dbConnection = new SqlConnection(CONNECTION_SQL_STR);
         }
     }
 }
