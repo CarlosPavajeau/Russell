f449ed85e385579539237e374ae6a14c2e54f00c
Carlos Pavajeau <cantte098@gmail.com>
2019-12-07 08:58:19 -0500

    Server: Some improvements

--- src/Server/DataPacketHandler.cs
+++ src/Server/DataPacketHandler.cs
@@ -8,18 +8,83 @@ namespace Server
     {
         public static object HandleDataPacket(DataPacket dataPacket)
         {
-            if (dataPacket.Command == Command.SEARCH_ADMINISTRATIVE_EMPLOYEE)
+            if (dataPacket.Command is SearchCommand searchCommand)
             {
-                AdministrativeEmployeeService administrativeEmployeeService = new AdministrativeEmployeeService();
-                AdministrativeEmployee administrativeEmployee = administrativeEmployeeService.Search(dataPacket.Data as string);
+                object data = HandleSearchCommand(searchCommand, dataPacket.Data as string);
 
-                if (administrativeEmployee is null)
+                if (data is null)
                     return ServerAnswer.NOT_FOUND_DATA;
-                else
-                    return administrativeEmployee;
+                else return data;
+            }
+            else if (dataPacket.Command is SaveCommand)
+            {
+                return HandleSaveCommand(dataPacket);
             }
             else
                 return ServerAnswer.NOT_FOUND_DATA;
+        }
+
+        private static ServerAnswer HandleSaveCommand(DataPacket dataPacket)
+        {
+            switch (dataPacket.Command.TypeData)
+            {
+                case TypeData.PERSON:
+                    PersonService personService = new PersonService();
+                    if (!personService.Save(dataPacket.Data as Person))
+                        return ServerAnswer.DATA_ALREADY_REGISTERED;
+                    return ServerAnswer.SAVE_SUCCESSFUL;
+                case TypeData.EMPLOYEE:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.ADMINISTRATIVE_EMPLOYEE:
+                    if (!new AdministrativeEmployeeService().Save(dataPacket.Data as AdministrativeEmployee))
+                        return ServerAnswer.DATA_ALREADY_REGISTERED;
+                    return ServerAnswer.SAVE_SUCCESSFUL;
+                case TypeData.BANKDRAFT:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.COMMEND:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.ROUTE:
+                    if (!new RouteService().Save(dataPacket.Data as Route))
+                        return ServerAnswer.DATA_ALREADY_REGISTERED;
+                    return ServerAnswer.SAVE_SUCCESSFUL;
+                case TypeData.TRANSPORT_FORM:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.VEHICLE:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.TICKET:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                default:
+                    return ServerAnswer.NOT_FOUND_DATA;
+            }
+        }
+
+        private static object HandleSearchCommand(SearchCommand searchCommand, string primaryKey)
+        {
+            switch (searchCommand.TypeData)
+            {
+                case TypeData.PERSON:
+                    PersonService personService = new PersonService();
+                    return personService.Search(primaryKey);
+                case TypeData.EMPLOYEE:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.ADMINISTRATIVE_EMPLOYEE:
+                    AdministrativeEmployeeService administrativeEmployeeService = new AdministrativeEmployeeService();
+                    return administrativeEmployeeService.Search(primaryKey);
+                case TypeData.BANKDRAFT:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.COMMEND:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.ROUTE:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.TRANSPORT_FORM:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.VEHICLE:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                case TypeData.TICKET:
+                    return ServerAnswer.NOT_FOUND_DATA;
+                default:
+                    return ServerAnswer.NOT_FOUND_DATA;
+            }
         }
     }
 }
