3fe39420439353e9bb937ff558e0566ca8dabf6b
Carlos Pavajeau <cantte098@gmail.com>
2019-12-08 12:27:06 -0500

    Add new project Infrastructure

---
+++ src/Infrastructure/Email.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Infrastructure
+{
+    public class Email
+    {
+        private MailMessage MailMessage;
+        private readonly SmtpClient Client;
+
+        public Email()
+        {
+            Client = new SmtpClient();
+        }
+
+        public async Task<bool> SendEmail(Employee employee)
+        {
+            try
+            {
+                ConfigSMTP();
+                ConfigEmail(employee);
+                await Client.SendMailAsync(MailMessage);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void ConfigSMTP()
+        {
+            Client.Host = "smtp.gmail.com";
+            Client.Port = 587;
+            Client.EnableSsl = true;
+            Client.UseDefaultCredentials = false;
+
+            Client.Credentials = new NetworkCredential("feres223ger@gmail.com", "olakease123");
+        }
+
+        private void ConfigEmail(Employee employee)
+        {
+            MailMessage = new MailMessage();
+            MailMessage.To.Add(employee.Email);
+            MailMessage.From = new MailAddress("feres223ger@gmail.com");
+
+            MailMessage.Subject = $"Bienvenido a la empresa. Señor {employee.Name} usted acaba de ser registrado como uno de nustros empleados el dia {DateTime.Now.ToShortDateString()}";
+            MailMessage.IsBodyHtml = false;
+            MailMessage.Priority = MailPriority.High;
+        }
+    }
+}
---
+++ src/Infrastructure/PDFDocument.cs
@@ -0,0 +1,28 @@
+﻿using Entity;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Infrastructure
+{
+    public class PDFDocument
+    {
+        public void SaveCommend(Commend commend, string path)
+        {
+            FileStream stream = new FileStream(path, FileMode.Create);
+            PdfDocument pdfDocument = new PdfDocument(new PdfWriter(stream));
+            Document document = new Document(pdfDocument);
+
+            Text title = new Text($"Emcomienda: {commend.Number}");
+            title.SetFontSize(30);
+            title.SetHorizontalAlignment(HorizontalAlignment.CENTER);
+            document.Add(new Paragraph(title));
+
+            document.Close();
+        }
+    }
+}
