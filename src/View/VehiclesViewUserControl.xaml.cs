67b4127322e0532b12a9fa5e4d4b11db7aa37dfc
Carlos Pavajeau <cantte098@gmail.com>
2019-12-08 12:48:42 -0500

    View: Add VehiclesUserControl

---
+++ src/View/VehiclesViewUserControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogicLayer;
+using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for VehiclesViewUserControl.xaml
+    /// </summary>
+    public partial class VehiclesViewUserControl : UserControl
+    {
+        public VehiclesViewUserControl()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            VehicleService vehicleService = new VehicleService();
+            Vehicles.ItemsSource = vehicleService.GetAllData();
+        }
+    }
+}
