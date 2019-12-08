fdd399cb5dabf8eb156b6f58966bbeb48f602c1f
Carlos Pavajeau <cantte098@gmail.com>
2019-12-08 12:46:09 -0500

    View: Add LittleVehiclesViewUserControl

---
+++ src/View/LittleVehiclesViewUserControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLogicLayer;
+using Entity;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for LittleVehiclesViewUserControl.xaml
+    /// </summary>
+    public partial class LittleVehiclesViewUserControl : UserControl
+    {
+        readonly IReception<Vehicle> _reception;
+        public LittleVehiclesViewUserControl()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            VehicleService vehicleService = new VehicleService();
+            Vehicles.ItemsSource = vehicleService.GetAllData();
+        }
+
+        public LittleVehiclesViewUserControl(IReception<Vehicle> reception) : this()
+        {
+            _reception = reception;
+        }
+
+        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            _reception?.Receive(Vehicles.SelectedItem as Vehicle);
+        }
+    }
+}
