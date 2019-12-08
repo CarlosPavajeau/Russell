ef1abbe5ad99f34f7c9441caa18bdfc580151d8c
Carlos Pavajeau <cantte098@gmail.com>
2019-12-07 20:25:11 -0500

    View: Add RegisterTicketUserControl #3

---
+++ src/View/RegisterTicketUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Entity;
+using System;
+using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for RegisterTicketUserControl.xaml
+    /// </summary>
+    public partial class RegisterTicketUserControl : UserControl
+    {
+        public RegisterTicketUserControl()
+        {
+            InitializeComponent();
+            TicketDate.Text += DateTime.Now.ToShortDateString();
+            TicketDispatcher.Text += MainWindow.AdministrativeEmployee.Name;
+        }
+
+        private void SearhPassenger_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            SelectPerson.Child = new PeopleViewUserControl(SetPassenger);
+            SelectPerson.IsOpen = true;
+        }
+
+        private void SetPassenger(Person person)
+        {
+            SelectPerson.IsOpen = false;
+            PassenderID.Text = person.ID;
+        }
+    }
+}
