c09db8e9a225c9c5f5bf14865f63a7260d9a8aaa
Carlos Pavajeau <cantte098@gmail.com>
2019-12-19 07:14:48 -0500

    Server/Log: Some improvements

--- src/Server/Log.cs
+++ src/Server/Log.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
 
 namespace Server
 {
     public static class Log
     {
+        const string LOG_FILE = "ServerLog.txt";
         public static void PrintMsg(string msg)
         {
             Console.WriteLine(msg);
+            SaveLog(msg);
+        }
+
+        private static void SaveLog(string msg)
+        {
+            using FileStream fileStream = new FileStream(@LOG_FILE, FileMode.Append, FileAccess.Write);
+            using StreamWriter streamWriter = new StreamWriter(fileStream);
+            streamWriter.WriteLineAsync(msg);
         }
     }
 }
