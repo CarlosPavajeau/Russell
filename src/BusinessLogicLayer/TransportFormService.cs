44d0725638aad896b2993c71daa013290d55b669
Carlos Pavajeau <cantte098@gmail.com>
2019-12-19 07:06:26 -0500

    TransportFormRepository-TransportFormService: Some improvements

--- src/BusinessLogicLayer/TransportFormService.cs
+++ src/BusinessLogicLayer/TransportFormService.cs
@@ -4,13 +4,50 @@ using System.Collections.Generic;
 
 namespace BusinessLogicLayer
 {
-    public class TransportFormService : Service, ISave<TransportForm>, ISave<Ticket>, ISearch<TransportForm>, IUpdate, IGetAllData<TransportForm>
+    public class TransportFormService : Service, ISave<TransportForm>, ISave<Ticket>, ISearch<TransportForm>, IUpdate, IGetAllData<TransportForm>, ICount
     {
         private readonly TransportFormRepository _transportFormRepository;
 
         public TransportFormService()
         {
             _transportFormRepository = new TransportFormRepository(dbConnection);
+        }
+
+        public int Count
+        {
+            get
+            {
+                try
+                {
+                    dbConnection.Open();
+                    return _transportFormRepository.Count;
+                }
+                catch (System.Exception)
+                {
+                    return 0;
+                }
+                finally
+                {
+                    dbConnection.Close();
+                }
+            }
+        }
+
+        public TransportForm CurrentTransportFrom(string dispatcherID)
+        {
+            try
+            {
+                dbConnection.Open();
+                return _transportFormRepository.CurrentTransportForm(dispatcherID);
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
         }
 
         public bool Save(TransportForm data)
@@ -62,6 +99,11 @@ namespace BusinessLogicLayer
             {
                 dbConnection.Close();
             }
+        }
+
+        public bool UpdateFinancialInformation(string transprotFormCode, FinalcialInformationType type, decimal newValue)
+        {
+            return Update(transprotFormCode, type.ToString(), newValue);
         }
 
         public bool Update(string primarykey, string columToModify, object newValue)
--- src/DataAccessLayer/TransportFormRepository.cs
+++ src/DataAccessLayer/TransportFormRepository.cs
@@ -7,14 +7,45 @@ using static Entity.FinalcialInformationType;
 
 namespace DataAccessLayer
 {
-    public class TransportFormRepository : Repository, ISave<TransportForm>, ISave<Ticket>, ISearch<TransportForm>, IUpdate, IMap<TransportForm>, IGetAllData<TransportForm>
+    public class TransportFormRepository : Repository, ISave<TransportForm>, ISave<Ticket>, ISearch<TransportForm>, IUpdate, IMap<TransportForm>, IGetAllData<TransportForm>, ICount
     {
-        static readonly string[] TRANSPORT_FORM_FIELDS = { "@transport_form_number", "@start_date", "@depature_time",
+        static readonly string[] TRANSPORT_FORM_FIELDS = { "@transport_form_number", "@state", "@start_date", "@depature_time",
                                                            "@value_of_tickets", "@total_value", "@license_plate", "@route_code", 
                                                            "@dispatcher"};
 
         private readonly FinalcialInformationRepository _finalcialInformationRepository;
         private readonly TicketRepository _ticketRepository;
+
+        public int Count
+        {
+            get
+            {
+                using (var command = dbConnection.CreateCommand())
+                {
+                    command.CommandText = "SELECT COUNT(*) FROM transport_forms";
+                    return (int)command.ExecuteScalar();
+                }
+            }
+        }
+
+        public TransportForm CurrentTransportForm(string dispatcherID)
+        {
+            using (var command = dbConnection.CreateCommand())
+            {
+                command.CommandText = "SELECT tf.transport_form_number, tf.state, tf.start_date, tf.depature_time, tf.value_of_tickets, " +
+                                      "tf.total_value, tf.license_plate, tf.route_code, tf.dispatcher, fi.replacement_fund, " +
+                                      "fi.social_contribution, fi.tire_service, fi.vehicle_fix_service, fi.non_contractual_secure_service, " +
+                                      "fi.constact_insurance_service, fi.social_protection, fi.extraordinary_protection, " +
+                                      "fi.administration, fi.others, fi.total FROM transport_forms tf " +
+                                      "JOIN finalcial_information fi ON tf.transport_form_number = fi.transport_form_number " +
+                                      "WHERE tf.state = 1 AND tf.dispatcher = @dispatcher";
+
+                command.Parameters.Add(CreateDbParameter(command, "@dispatcher", dispatcherID));
+
+                using (var dbDataReader = command.ExecuteReader())
+                    return dbDataReader.Read() ? Map(dbDataReader) : null;
+            }
+        }
 
         public TransportFormRepository(DbConnection connection) : base(connection)
         {
@@ -26,15 +57,16 @@ namespace DataAccessLayer
         {
             using (var command = dbConnection.CreateCommand())
             {
-                command.CommandText = "INSERT INTO transport_forms(transport_form_number, start_date, depature_time, " +
+                command.CommandText = "INSERT INTO transport_forms(transport_form_number, state, start_date, depature_time, " +
                                       "value_of_tickets, total_value, license_plate, route_code, dispatcher) " +
-                                      "VALUES(@transport_form_number, @start_date, @depature_time, " +
+                                      "VALUES(@transport_form_number, @state, @start_date, @depature_time, " +
                                              "@value_of_tickets, @total_value, @license_plate, @route_code, @dispatcher)";
 
                 MapCommandParameters(command, TRANSPORT_FORM_FIELDS,
                     new object[]
                     {
                         data.Number,
+                        data.State,
                         data.Date,
                         data.DepartureTime,
                         data.ValueOfTickets,
@@ -58,7 +90,7 @@ namespace DataAccessLayer
         {
             using (var command = dbConnection.CreateCommand())
             {
-                command.CommandText = "SELECT tf.transport_form_number, tf.start_date, tf.depature_time, tf.value_of_tickets, " +
+                command.CommandText = "SELECT tf.transport_form_number, tf.state, tf.start_date, tf.depature_time, tf.value_of_tickets, " +
                                       "tf.total_value, tf.license_plate, tf.route_code, tf.dispatcher, fi.replacement_fund, " +
                                       "fi.social_contribution, fi.tire_service, fi.vehicle_fix_service, fi.non_contractual_secure_service, " +
                                       "fi.constact_insurance_service, fi.social_protection, fi.extraordinary_protection, " +
@@ -76,34 +108,36 @@ namespace DataAccessLayer
         public TransportForm Map(DbDataReader dbDataReader)
         {
             AdministrativeEmployeeRepository administrativeEmployeeRepository = new AdministrativeEmployeeRepository(dbConnection);
-            AdministrativeEmployee dispatcher = administrativeEmployeeRepository.Search(dbDataReader.GetString(7), true);
+            AdministrativeEmployee dispatcher = administrativeEmployeeRepository.Search(dbDataReader.GetString(8), true);
             VehicleRepository vehicleRepository = new VehicleRepository(dbConnection);
-            Vehicle vehicle = vehicleRepository.Search(dbDataReader.GetString(5));
+            Vehicle vehicle = vehicleRepository.Search(dbDataReader.GetString(6));
             RouteRepository routeRepository = new RouteRepository(dbConnection);
-            Route route = routeRepository.Search(dbDataReader.GetString(6));
+            Route route = routeRepository.Search(dbDataReader.GetString(7));
 
             if (dispatcher is null || vehicle is null || route is null)
                 return null;
 
             string transport_form_number = dbDataReader.GetString(0);
+            bool state = dbDataReader.GetBoolean(1);
             DateTime startDate, depatureTime;
 
-            startDate = dbDataReader.GetDateTime(1);
-            depatureTime = dbDataReader.GetDateTime(2);
+            startDate = dbDataReader.GetDateTime(2);
 
-            TransportForm transportForm = new TransportForm(transport_form_number, route, vehicle, dispatcher, startDate, depatureTime);
+            depatureTime = dbDataReader.GetDateTime(3);
+
+            TransportForm transportForm = new TransportForm(transport_form_number, route, vehicle, dispatcher, startDate, depatureTime, state);
 
             
-            transportForm.AddFinalcialInformation(REPLACEMENT_FUND, dbDataReader.GetDecimal(8));
-            transportForm.AddFinalcialInformation(SOCIAL_CONTRIBUTION, dbDataReader.GetDecimal(9));
-            transportForm.AddFinalcialInformation(TIRE_SERVICE, dbDataReader.GetDecimal(10));
-            transportForm.AddFinalcialInformation(VEHICLE_FIX_SERVICE, dbDataReader.GetDecimal(11));
-            transportForm.AddFinalcialInformation(NON_CONTRACTUAL_SERCURE_SERVICE, dbDataReader.GetDecimal(12));
-            transportForm.AddFinalcialInformation(CONSTACT_INSURANCE_SERVICE, dbDataReader.GetDecimal(13));
-            transportForm.AddFinalcialInformation(SOCIAL_PROTECTION, dbDataReader.GetDecimal(14));
-            transportForm.AddFinalcialInformation(EXTRAORDINARY_PROTECTION, dbDataReader.GetDecimal(15));
-            transportForm.AddFinalcialInformation(ADMINISTRATION, dbDataReader.GetDecimal(16));
-            transportForm.AddFinalcialInformation(OTHERS, dbDataReader.GetDecimal(17));
+            transportForm.AddFinalcialInformation(REPLACEMENT_FUND, dbDataReader.GetDecimal(9));
+            transportForm.AddFinalcialInformation(SOCIAL_CONTRIBUTION, dbDataReader.GetDecimal(10));
+            transportForm.AddFinalcialInformation(TIRE_SERVICE, dbDataReader.GetDecimal(11));
+            transportForm.AddFinalcialInformation(VEHICLE_FIX_SERVICE, dbDataReader.GetDecimal(12));
+            transportForm.AddFinalcialInformation(NON_CONTRACTUAL_SERCURE_SERVICE, dbDataReader.GetDecimal(13));
+            transportForm.AddFinalcialInformation(CONSTACT_INSURANCE_SERVICE, dbDataReader.GetDecimal(14));
+            transportForm.AddFinalcialInformation(SOCIAL_PROTECTION, dbDataReader.GetDecimal(15));
+            transportForm.AddFinalcialInformation(EXTRAORDINARY_PROTECTION, dbDataReader.GetDecimal(16));
+            transportForm.AddFinalcialInformation(ADMINISTRATION, dbDataReader.GetDecimal(17));
+            transportForm.AddFinalcialInformation(OTHERS, dbDataReader.GetDecimal(18));
             transportForm.UpdateTotalValue();
 
             using (var ticketCommand = dbConnection.CreateCommand())
@@ -118,10 +152,10 @@ namespace DataAccessLayer
                 {
                     PersonRepository personRepository = new PersonRepository(dbConnection);
 
-                    if (!(personRepository.Search(ticketDataReader.GetString(2)) is Passenger passenger))
+                    if (!(personRepository.Search(ticketDataReader.GetString(2)) is Person passenger))
                         continue;
 
-                    transportForm.AddTicket(passenger, ticketDataReader.GetInt32(0), ticketDataReader.GetDateTime(1));
+                    transportForm.AddTicket(Person.ToPassenger(passenger), ticketDataReader.GetInt16(0), ticketDataReader.GetDateTime(1));
                 }
             }
 
@@ -130,7 +164,27 @@ namespace DataAccessLayer
 
         public bool Update(string primarykey, string columToModify, object newValue)
         {
-            throw new System.NotImplementedException();
+            if (IsFinalcialInformationColum(columToModify))
+                return _finalcialInformationRepository.Update(primarykey, columToModify, newValue);
+
+            using (var command = dbConnection.CreateCommand())
+            {
+                command.CommandText = $"UPDATE transport_forms SET { columToModify } = @newValue WHERE transport_form_number = @transport_form_number";
+
+                command.Parameters.Add(CreateDbParameter(command, "@newValue", newValue));
+                command.Parameters.Add(CreateDbParameter(command, "@transport_form_number", primarykey));
+
+                return command.ExecuteNonQuery() > 0;
+            }
+        }
+
+        private bool IsFinalcialInformationColum(string colum)
+        {
+            string[] FINANCIAL_INFORMATION_COLUMS = { "replacement_fund", "social_contribution", "tire_service", "vehicle_fix_service",
+                                                      "non_contractual_secure_service", "constact_insurance_service", "social_protection",
+                                                      "extraordinary_protection", "administration", "others" };
+
+            return Array.Exists(FINANCIAL_INFORMATION_COLUMS, cl => cl == colum);
         }
 
         public IList<TransportForm> GetAllData()
@@ -139,7 +193,7 @@ namespace DataAccessLayer
 
             using (var command = dbConnection.CreateCommand())
             {
-                command.CommandText = "SELECT tf.transport_form_number, tf.start_date, tf.depature_time, tf.value_of_tickets, " +
+                command.CommandText = "SELECT tf.transport_form_number, tf.state, tf.start_date, tf.depature_time, tf.value_of_tickets, " +
                                       "tf.total_value, tf.license_plate, tf.route_code, tf.dispatcher, fi.replacement_fund, " +
                                       "fi.social_contribution, fi.tire_service, fi.vehicle_fix_service, fi.non_contractual_secure_service, " +
                                       "fi.constact_insurance_service, fi.social_protection, fi.extraordinary_protection, " +
@@ -166,7 +220,15 @@ namespace DataAccessLayer
 
             public bool Update(string primarykey, string columToModify, object newValue)
             {
-                throw new System.NotImplementedException();
+                using (var command = dbConnection.CreateCommand())
+                {
+                    command.CommandText = $"UPDATE finalcial_information SET { columToModify } = @newValue WHERE transport_form_number = @transport_form_number";
+
+                    command.Parameters.Add(CreateDbParameter(command, "@newValue", newValue));
+                    command.Parameters.Add(CreateDbParameter(command, "@transport_form_number", primarykey));
+
+                    return command.ExecuteNonQuery() > 0;
+                }
             }
         }
 
