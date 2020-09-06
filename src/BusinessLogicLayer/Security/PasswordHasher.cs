f114ec413f34736dbc8a42c813798a009febea30
Carlos Pavajeau <cantte098@gmail.com>
2020-09-06 17:08:55 -0500

    Add PasswordHasher

---
+++ src/BusinessLogicLayer/Security/PasswordHasher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BusinessLogicLayer.Security
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashedBytes = sha256.ComputeHash(passwordBytes);
+
+            return Convert.ToBase64String(hashedBytes);
+        }
+
+        public static bool VerifyPassword(string hashedPassword, string password)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            SHA256 sha256 = SHA256.Create();
+            passwordBytes = sha256.ComputeHash(passwordBytes);
+
+            return Convert.ToBase64String(passwordBytes) == hashedPassword;
+        }
+    }
+}
